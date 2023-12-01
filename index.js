a09e6b80248b6913ab319c974926d9cf6a8283e5
Enzo <99659392+Kvarzsiev@users.noreply.github.com>
2023-12-01 20:33:02 -0300

    Pim Pom Pam

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let authorization;
 
-$(document).ready(function () {
+$(document).ready(function() {
   authenticate();
 });
 
 function authenticate() {
   $.post({
-    url: "api_url/Autenticar",
+    url: "https://apitesteucs.azurewebsites.net/API/Autenticar",
     data: {
-      chaveAPI: "key",
+      chaveAPI: "8175f301022f475daa5e6098c532a2aa",
     },
-    success: function (result) {
+    success: function(result) {
       authorization = result;
       loadImages();
     },
@@ -21,28 +21,26 @@ function loadImages() {
   // Render the images dynamically
   for (var i = 0; i < 5; i++) {
     $.post({
-      url: "api_url/ObterImagem",
+      url: "https://apitesteucs.azurewebsites.net/API/ObterImagem",
       headers: {
         authUCSJWT: authorization,
       },
-      success: function (result) {
+      success: function(result) {
         console.log("image: ", result);
-        var img = new Image(window.innerWidth * 0.3);
+        var img = new Image(window.innerWidth * 0.5);
 
         img.src = "data:image/png;base64," + result;
 
-        img.onload = function () {
+        img.onload = function() {
           var container = document.getElementById("imageContainer");
           container.appendChild(img);
         };
-
-        console.log("result: ", result);
       },
     });
   }
 }
 
-$(window).scroll(function () {
+$(window).scroll(function() {
   if ($(window).scrollTop() + $(window).height() == $(document).height()) {
     loadImages();
   }
